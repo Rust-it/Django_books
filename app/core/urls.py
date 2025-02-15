ae440f9dcf7152c6833cbbccab5336a8e3254514
Rust-it <rust251@yandex.ru>
2025-02-15 11:27:05 +0300

    Add project Django_books

---
+++ app/core/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('rosetta/', include('rosetta.urls')),
+    path('', include('books.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
